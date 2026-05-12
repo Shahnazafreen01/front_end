7af1a9230316946df48b929232c0f8c5ddd2e7d2
Shahnazafreen01 <shahnazafreen480@gmail.com>
2026-05-12 16:10:37 +0530

    Add files via upload

---
+++ JavaScript/modules.js
@@ -0,0 +1,12 @@
+export function multiply(x,y){
+    return x * y;
+
+}
+
+export function a(){
+   let p =  prompt("Enter your name : ");
+   console.log(`My name is: ${p}`);
+   alert(`My name is: ${p}`);
+
+
+} 
